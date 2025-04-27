bc6fadd55ca2ce4d3784ae5ec8ef42baeafc9072
BilalWhiteHat <muhammadbilalkhan122@gmail.com>
2025-04-27 11:45:40 +0500

    add postcss and tailwind configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// filepath: /home/dev/Desktop/www/cam_frontend-vue/tailwind.config.js
+module.exports = {
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
